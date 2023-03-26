0981b843646757cffa4498c16f86312f40d60cea
AIex05 <78372456+AIex05@users.noreply.github.com>
2023-03-26 16:28:47 -0400

    Template completed

--- src/l1edges.java
+++ src/l1edges.java
@@ -3,9 +3,13 @@ public class l1edges {
     String startNode;
     String endNode;
 
+    static void DisplayEdge(l1edges Edge){
+        System.out.println("ID: "+Edge.edgeID+"\s"+"startNode: "+Edge.startNode+"\s"+"endNode: "+Edge.endNode+"\n"+"----------------------------------------------");
+    }
+
     public l1edges(String edgeID, String startNode, String endNode) {
-        edgeID = edgeID;
-        startNode = startNode;
-        endNode = endNode;
+        this.edgeID = edgeID;
+        this.startNode = startNode;
+        this.endNode = endNode;
     }
 }

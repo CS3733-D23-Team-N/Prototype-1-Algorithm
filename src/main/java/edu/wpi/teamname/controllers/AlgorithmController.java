e3ad41df6d22c052fad4f912c439d3b61e350962
krlepping <krlepping@wpi.edu>
2023-03-27 19:10:51 -0400

    CSV parsing

--- src/main/java/edu/wpi/teamname/controllers/AlgorithmController.java
+++ src/main/java/edu/wpi/teamname/controllers/AlgorithmController.java
@@ -2,14 +2,35 @@ package edu.wpi.teamname.controllers;
 
 import edu.wpi.teamname.entity.Node;
 import java.util.ArrayList;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 
 public class AlgorithmController {
 
     private ArrayList<Node> nodes;
 
-    public AlgorithmController() {
+    public AlgorithmController(String nodeFile) {
         // Add all information from CSV files into the nodes ArrayList, including all edge relationships
         //TODO: parse CSV and stuff
+        String line = "";
+        String splitBy = ",";
+        try
+        {
+            BufferedReader br = new BufferedReader(new FileReader(nodeFile));
+            while ((line = br.readLine()) != null)   //returns a Boolean value
+            {
+                String[] nodeInfo = line.split(splitBy);
+                int xcoord = Integer.parseInt(nodeInfo[1]);
+                int ycoord = Integer.parseInt(nodeInfo[2]);
+
+                Node thisNode = new Node(nodeInfo[0], nodeInfo[3], nodeInfo[4], nodeInfo[5], nodeInfo[6], nodeInfo[7], xcoord, ycoord);
+
+            }
+        }
+        catch (IOException e){
+
+        }
     }
 
     /**

8cb2878d2dc19ed85b3cfb67bc15775e711ff621
Harshith Iyer <harbar2021@gmail.com>
2023-03-27 23:14:56 -0400

    coded rootcontroller and root.fxml

--- src/main/java/edu/wpi/niftynymphs/controllers/RootController.java
+++ src/main/java/edu/wpi/niftynymphs/controllers/RootController.java
@@ -1,8 +1,40 @@
 package edu.wpi.niftynymphs.controllers;
 
+import edu.wpi.niftynymphs.navigation.Navigation;
+import edu.wpi.niftynymphs.navigation.Screen;
+import javafx.application.Platform;
 import javafx.fxml.FXML;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
 
 public class RootController {
+
+  // Panes
+  @FXML GridPane outerGridPane;
+  @FXML GridPane innerGridPane;
+  @FXML BorderPane innerBorderPane;
+
+  // Images in nav bar
+  @FXML ImageView homeLogo;
+  @FXML ImageView helpIcon;
+  @FXML ImageView exitIcon;
+
+  @FXML
+  public void navigateHome() {
+    Navigation.navigate(Screen.HOME);
+  }
+
+  @FXML
+  public void navigateHelp() {
+    Navigation.navigate(Screen.HELP);
+  }
+
+  @FXML
+  public void exitApp() {
+    Platform.exit();
+  }
+
   @FXML
   public void initialize() {}
 }
--- src/main/java/edu/wpi/niftynymphs/navigation/Screen.java
+++ src/main/java/edu/wpi/niftynymphs/navigation/Screen.java
@@ -3,6 +3,7 @@ package edu.wpi.niftynymphs.navigation;
 public enum Screen {
   ROOT("views/Root.fxml"),
   HOME("views/Home.fxml"),
+  HELP("views/Help.fxml"),
   SERVICE_REQUEST("views/ServiceRequest.fxml");
 
   private final String filename;

650e90fa60af8a5a705da7432c2d85a8c4112eb8
crueda2025 <cody.m.rueda@gmail.com>
2023-03-27 23:06:21 -0400

    Finished Signage Page

--- src/main/java/edu/wpi/niftynymphs/Main.java
+++ src/main/java/edu/wpi/niftynymphs/Main.java
@@ -3,6 +3,7 @@ package edu.wpi.niftynymphs;
 public class Main {
 
   public static void main(String[] args) {
+    System.out.println("This working?");
     App.launch(App.class, args);
   }
 
---
+++ src/main/java/edu/wpi/niftynymphs/controllers/SignageController.java
@@ -0,0 +1,4 @@
+package edu.wpi.niftynymphs.controllers;
+
+public class SignageController {
+}
